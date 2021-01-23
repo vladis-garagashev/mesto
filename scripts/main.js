7eebe67aa078a57bb3fa09fd9d5bae344013d23d
Vladislav Garagashev <vladis.garagashev@yandex.ru>
2021-01-23 15:08:23 +0500

    fix: Изменить порядок написания кода JS

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,17 @@ let popup = document.querySelector('.popup');
 // Находим кнопки открыть и закрыть в DOM
 let popupOpenButton = document.querySelector('.button_type_edit');
 let popupCloseButton = popup.querySelector('.form__button_type_close');
+// Находим форму в DOM
+let formElement = document.querySelector('.form');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.form__item_el_name');
+let jobInput = formElement.querySelector('.form__item_el_job');
+// Выберите элементы, куда должны быть вставлены значения полей
+let profileName = document.querySelector('.profile__name')
+let profileJob = document.querySelector('.profile__job');
+// Вставляем старые значения в инпуты
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
 
 // Обработчик открытия и закрытия попапа
 function togglePopup() {
@@ -22,26 +33,6 @@ function closePopup(evt) {
     togglePopup();
   }
 }
-
-// Слушатели
-popupOpenButton.addEventListener('click', togglePopup);
-popupCloseButton.addEventListener('click', togglePopup);
-popup.addEventListener('click', closePopup);
-
-/* ------------------------------------------------------- */
-
-// Находим форму в DOM
-let formElement = document.querySelector('.form');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.form__item_el_name');
-let jobInput = formElement.querySelector('.form__item_el_job');
-// Выберите элементы, куда должны быть вставлены значения полей
-let profileName = document.querySelector('.profile__name')
-let profileJob = document.querySelector('.profile__job');
-// Вставляем старые значения в инпуты
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
-
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler(evt) {
@@ -58,6 +49,14 @@ function formSubmitHandler(evt) {
 
 }
 
+// Слушатели открытия и закрытия попапа
+popupOpenButton.addEventListener('click', togglePopup);
+popupCloseButton.addEventListener('click', togglePopup);
+popup.addEventListener('click', closePopup);
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
